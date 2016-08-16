b9f4d20a0cebe8c2d8ddf901e4cc1debd3914df5
Hao Dong <dhsig552@163.com>
2016-08-16 14:13:28 -0400

    activation py 翻译

--- tensorlayer/activation.py
+++ tensorlayer/activation.py
@@ -6,27 +6,27 @@
 import tensorflow as tf
 
 def identity(x):
-    """The identity activation function
+    """线性激活函数
 
-    Parameters
+    参数
     ----------
-    x : a tensor outputs
+    x : Tensor输入
         input(s)
     """
     return x
 
 def ramp(x=None, v_min=0, v_max=1, name=None):
-    """The ramp activation function.
+    """斜坡激活函数
 
-    Parameters
+    参数
     ----------
-    x : a tensor outputs
+    x : Tensor输入
         input(s)
-    v_min : float
+    v_min : 浮点数
         if input(s) smaller than v_min, change inputs to v_min
-    v_max : float
+    v_max : 浮点数
         if input(s) greater than v_max, change inputs to v_max
-    name : a string or None
-        An optional name to attach to this activation function.
+    name : 字符串或 None
+        可选输入，该激活函数的名字
     """
     return tf.clip_by_value(x, clip_value_min=v_min, clip_value_max=v_max, name=name)
