d00698ac4b164b526ea7da6c888cd125e26676b6
Luo Mai <luo.mai.cs@gmail.com>
2018-06-08 11:32:00 +0800

    check in setup.py to fix doc build error.

---
+++ setup.py
@@ -0,0 +1,24 @@
+-from setuptools import setup, find_packages
+-
+-install_requires = [
+-    'numpy',
+-    # 'tensorflow', # user install it
+-    'scipy',
+-    'scikit-image',
+-    'matplotlib',
+-]
+-
+-setup(
+-    name = "tensorlayer",
+-    version = "1.8.5rc2",
+-    include_package_data=True,
+-    author='TensorLayer Contributors',
+-    author_email='hao.dong11@imperial.ac.uk',
+-    url = "https://github.com/tensorlayer/tensorlayer" ,
+-    license = "Apache 2.0" ,
+-    packages = find_packages(),
+-    install_requires=install_requires,
+-    description = "A TensorFlow-based Deep Learning Library for Researchers and Engineers.",
+-    keywords = "Deep learning, Reinforcement learning, TensorFlow",
+-    platform=['any'],
+-)
